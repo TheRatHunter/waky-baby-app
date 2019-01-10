6b1b689f05a50c4c5f6a33e95aad51f316f318a4
Damien-NICOLAS <dmien.nicols@gmail.com>
2019-01-10 08:41:58 +0100

    Preparation to use BLE-manager

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Waky Baby
 
 React native mobile application for Waky Baby, a babyphone for deaf and hearing impaired.
+
+To install the BLE library :
+
+npm i --save react-native-ble-manager
+
+Then :
+
+react-native link react-native-ble-manager
+
+The edition of the build.gradle is already done.
+
+See https://github.com/innoveit/react-native-ble-manager for more information
--- android/app/src/main/java/com/wakybaby/MainApplication.java
+++ android/app/src/main/java/com/wakybaby/MainApplication.java
@@ -3,6 +3,7 @@ package com.wakybaby;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import it.innove.BleManagerPackage;
 import com.cubicphuse.RCTTorch.RCTTorchPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BleManagerPackage(),
             new RCTTorchPackage(),
             new RNGestureHandlerPackage()
       );
